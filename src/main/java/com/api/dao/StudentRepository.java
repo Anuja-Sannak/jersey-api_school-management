8174cc019be8016a5e9226f4c6a71c20cd5853bb
Anuja-Sannak <anuja.sannak03@gmail.com>
2025-08-20 11:19:37 +0530

    some student entity changes

--- src/main/java/com/api/dao/StudentRepository.java
+++ src/main/java/com/api/dao/StudentRepository.java
@@ -19,13 +19,12 @@ public class StudentRepository {
         return student;
     }
 
+
     public Student findById(int id) {
         EntityManager em = EMFProvider.getEntityManager();
         Student student = em.createQuery(
-            "SELECT s FROM Student s " +
-            "LEFT JOIN FETCH s.teacher " +
-            "LEFT JOIN FETCH s.subject " +
-            "WHERE s.student_id = :id", Student.class)
+            "SELECT s FROM Student s LEFT JOIN FETCH s.teacher t LEFT JOIN FETCH t.subject LEFT JOIN FETCH s.subject "
+            + "WHERE s.student_id = :id", Student.class)
             .setParameter("id", id)
             .getSingleResult();
         em.close();
@@ -36,10 +35,8 @@ public class StudentRepository {
     public List<Student> findAll() {
         EntityManager em = EMFProvider.getEntityManager();
         List<Student> list = em.createQuery(
-            "SELECT DISTINCT s FROM Student s " +
-            "LEFT JOIN FETCH s.teacher t " +
-            "LEFT JOIN FETCH t.subject " + // fetch subjects of teachers
-            "LEFT JOIN FETCH s.subject", Student.class) // fetch student subjects
+            "SELECT DISTINCT s FROM Student s LEFT JOIN FETCH s.teacher t LEFT JOIN FETCH t.subject "
+            + "LEFT JOIN FETCH s.subject", Student.class) 
             .getResultList();
         em.close();
         return list;
@@ -49,10 +46,8 @@ public class StudentRepository {
     public Student findByName(String name) {
         EntityManager em = EMFProvider.getEntityManager();
         List<Student> list = em.createQuery(
-            "SELECT s FROM Student s " +
-            "LEFT JOIN FETCH s.teacher " +
-            "LEFT JOIN FETCH s.subject " +
-            "WHERE s.name = :name", Student.class)
+            "SELECT s FROM Student s LEFT JOIN FETCH s.teacher LEFT JOIN FETCH s.subject "
+            + "WHERE s.name = :name", Student.class)
             .setParameter("name", name)
             .getResultList();
         em.close();
